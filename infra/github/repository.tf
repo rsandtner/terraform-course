60bddceb6de9415948f9b35a40224f239eff255c
Reinhard Sandtner <rsandtner@apache.org>
2025-08-03 23:36:31 +0200

    configure course repository

--- infra/github/repository.tf
+++ infra/github/repository.tf
@@ -1,7 +1,7 @@
 resource "github_repository" "github_course_repo" {
   name = "terraform-course"
   description = "Terraform course repository"
-  visibility = "private"
+  visibility = "public"
 }
 
 resource "github_branch" "github_course_repo_main_branch" {
@@ -13,4 +13,24 @@ resource "github_branch" "github_course_repo_main_branch" {
 resource "github_branch_default" "github_course_repo_default_branch" {
   repository = github_repository.github_course_repo.id
   branch = github_branch.github_course_repo_main_branch.branch
+}
+
+resource "github_repository_ruleset" "github_course_repo_main_ruleset" {
+  name        = "protect_default_branch"
+  repository  = github_repository.github_course_repo.id
+  target      = "branch"
+  enforcement = "active"
+
+  conditions {
+    ref_name {
+      include = [ "~DEFAULT_BRANCH" ]
+      exclude = []
+    }
+  }
+
+  rules {
+    deletion                = true
+    required_linear_history = true
+    non_fast_forward = true
+  }
 }
